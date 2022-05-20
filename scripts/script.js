db46ac546164caf63a91d80fbee97b5cfbcdfe96
Marlon Vivas <mj7marlonvivas@gmail.com>
2022-05-20 17:11:09 -0500

    mostrar terminado

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 import mostrarInfo from "../modules/mostrarInfo.js";
 
-let contenedor = document.getElementById('id');
+let contenedor = document.getElementById('mostrar');
 let but = document.getElementById('btn');
 let agg = document.getElementById('agg')
 let carrito = document.getElementById('carrito')
@@ -15,40 +15,40 @@ document.addEventListener('DOMContentLoaded', async ({}) => {
 });
 
 
-agg.addEventListener('submit', async () => {
+// agg.addEventListener('submit', async () => {
 
-    let input1 = document.getElementById('valor1').value;
-    let input2 = document.getElementById('valor2').value;
-    let input3 = document.getElementById('valor3').value;
+//     let input1 = document.getElementById('valor1').value;
+//     let input2 = document.getElementById('valor2').value;
+//     let input3 = document.getElementById('valor3').value;
 
-    let newObjet = {
-        name: input1,
-        img: input2,
-        description: input3
-    }
+//     let newObjet = {
+//         name: input1,
+//         img: input2,
+//         description: input3
+//     }
 
-    await axios.post(URL, newObjet);
+//     await axios.post(URL, newObjet);
 
-});
+// });
 
-carrito.addEventListener('click', ({target})=>{
-    if (target.classList.contains(carrito)) {
-        let id = target.id;
-        let capElemento = data.find((item) => item.id == id);
-        localStorage.setItem('newElemento', JSON.stringify(capElemento));
-    }
-})
-
-
+// carrito.addEventListener('click', ({target})=>{
+//     if (target.classList.contains(carrito)) {
+//         let id = target.id;
+//         let capElemento = data.find((item) => item.id == id);
+//         localStorage.setItem('newElemento', JSON.stringify(capElemento));
+//     }
+// })
 
 
-but.addEventListener('click', async () => {
-
-    await axios.delete(urlEmpoin);
-
-});
 
 
-const put = async (urlEmpoin, objeto = {}) => {
-    await axios.put(urlEmpoin, objeto);
-};
+// but.addEventListener('click', async () => {
+
+//     await axios.delete(urlEmpoin);
+
+// });
+
+
+// const put = async (urlEmpoin, objeto = {}) => {
+//     await axios.put(urlEmpoin, objeto);
+// };
